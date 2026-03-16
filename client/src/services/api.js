ded72f845d47374b1686e33ee2dd289bff82b26c
kanupriyakaushik <kaushikkanupriya81@gmail.com>
2026-03-16 12:04:08 +0530

    Initial commit - task manager

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,13 +4,4 @@ const API = axios.create({
   baseURL: "http://127.0.0.1:5000/api",
 });
 
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    req.headers = req.headers || {};
-    req.headers.Authorization = `Bearer ${token}`;
-  }
-  return req;
-});
-
 export default API;
